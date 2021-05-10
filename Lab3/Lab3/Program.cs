7d0af362af35bac1a710a89a4192f81a55e50155
SergeyLytvynenko <78361696+SergeyLytvynenko@users.noreply.github.com>
2021-05-10 19:42:05 +0300

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        class myThread
+        {
+            Thread thread;
+
+            public myThread(string name, int num) 
+            {
+
+                thread = new Thread(this.func);
+                thread.Name = name;
+                thread.Start(num);
+            }
+
+            void func(object num)
+            {
+                for (int i = 0; i < (int)num; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " выводит " + i);
+                    Thread.Sleep(0);
+                }
+                Console.WriteLine(Thread.CurrentThread.Name + " завершился");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            myThread t1 = new myThread("Thread 1", 6);
+            myThread t2 = new myThread("Thread 2", 3);
+            myThread t3 = new myThread("Thread 3", 2);
+
+            Console.Read();
+
+        }
+
+    }
+}
